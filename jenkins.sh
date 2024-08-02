30e92e03a5abc70315e3336c12cdc8bb527b5ea0
devaraj <devum456@gmail.com>
2024-08-02 08:17:35 +0000

    jenkins file

---
+++ jenkins.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+# update the system
+sudo apt update
+
+#java --version
+
+
+# install java dependency to install jenkins
+
+apt install openjdk-21-jre-headless -y
+   
+# java --version
+  
+#Add the repository key to the system
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc   https://pkg.jenkins.io/debian/jenkins.io-2023.key
+
+
+# After the key is added the system will return with OK. Next, let’s append the Debian package repository address to the server’s sources.list
+
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# update again
+sudo apt-get update
+ 
+#install jenkins
+sudo apt-get install jenkins -y
+
+#start jenkins
+sudo systemctl start jenkins
+     
+#sudo systemctl status jenkins
+
+
+# After the key is added the system will return with OK. Next, let’s append the Debian package repository address to the server’s sources.list
+sudo ufw allow 8080
+
+#  If the firewall is inactive, the following commands will allow OpenSSH and enable the firewall
+     
+sudo ufw allow OpenSSH
+
+#
+sudo ufw enable
+     
+# sudo ufw status
